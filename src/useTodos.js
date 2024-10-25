bd00d5c21d131983225340247ab16fb7eddeeaa0
YMarianaAguilar <ymarianaaguilar@gmail.com>
2024-10-24 23:58:19 -0300

    Added task saving with localStorage

--- src/useTodos.js
+++ src/useTodos.js
@@ -1,25 +1,36 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export const useTodos = () => {
-  const [todos, setTodos] = useState([]);
-  const [editingTodo, setEditingTodo] = useState(null);  // Estado para almacenar la tarea a editar
 
+  const initialTodos = JSON.parse(localStorage.getItem('todos')) || [];
+  
+  const [todos, setTodos] = useState(initialTodos); // Cargar los todos desde localStorage al iniciar 
+  const [editingTodo, setEditingTodo] = useState(null); //Estado para las tareas
+  
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]); //Actualizar localStorage cada vez que el estado de todos cambie
+
+  //Función para agregar una nueva tarea
   const addTodo = (todo) => {
-    setTodos([...todos, { id: Date.now(), text: todo }]);
+    const newTodo = { id: Date.now(), text: todo };
+    setTodos([...todos, newTodo]);
   };
 
+  //Función para eliminar una tarea
   const removeTodo = (id) => {
     setTodos(todos.filter(todo => todo.id !== id));
   };
 
+  //Función para actualizar (editar) una tarea
   const updateTodo = (id, newText) => {
     setTodos(todos.map(todo => (todo.id === id ? { ...todo, text: newText } : todo)));
-    setEditingTodo(null);  // Reiniciar el estado de edición después de actualizar
+    setEditingTodo(null);
   };
 
+  //Almacenar la tarea que estamos editando
   const startEditing = (todo) => {
-    console.log('Editando tarea:', todo);
-    setEditingTodo(todo);  // Almacenar la tarea que estamos editando
+    setEditingTodo(todo);
   };
 
   return { todos, addTodo, removeTodo, updateTodo, editingTodo, startEditing };
